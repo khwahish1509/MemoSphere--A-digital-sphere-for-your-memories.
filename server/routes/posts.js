547254d854d04801069e775bfba4972ff7119c05
Khwahish <khwahishvaid07@gmail.com>
2025-01-02 00:32:49 -0500

    Done with Day1

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {getPosts} from '../controllers/posts.js';
+import {getPosts, createPost} from '../controllers/posts.js';
 import { get } from 'mongoose';
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost);
+
 
 export default router;
