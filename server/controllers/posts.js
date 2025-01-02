dc54a8a83b917cbb4e96a4df513cb6f9ce6f6a2c
Khwahish <khwahishvaid07@gmail.com>
2025-01-02 15:36:11 -0500

    Moving To Client Side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,31 @@
-export const getPosts = (req, res) => {
-    res.send('This works!');
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        // Fetch all the post messages from the database
+        const postMessage = await PostMessage.find();
+        console.log(postMessage);
+
+        // Send the post messages as a response
+        res.status(200).json(postMessage);
+    } catch (error) {
+        res.status(404).json({ message: error.message });   
+    }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    // Get the post message from the request body
+    const post = req.body;
+    // Create a new post message using the PostMessage model
+    const newPost = new PostMessage(post);
+     try {
+        // Save the new post message to the database
+        await newPost.save();
+        // Send the new post message as a response
+        res.status(201).json(newPost);
+        
+     } catch (error) {
+        // Send an error response if the post message could not be saved.
+        res.status(409).json({ message: error.message });
+     }
 }
