63106cc08f9fe4069953e49900a740a11301fa3d
Khwahish <khwahishvaid07@gmail.com>
2025-01-02 00:30:21 -0500

    Design of schema model

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 import express from 'express';
-
+import {getPosts} from '../controllers/posts.js';
+import { get } from 'mongoose';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('This works!');
-}); 
+router.get('/', getPosts);
 
 export default router;
